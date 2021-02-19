d1b4c07b628daca413ef932bfd93836ddee5dbbf
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2021-02-19 14:42:48 +0800

    Revised. trie structure - ipv4 in ipv6 node

--- lpm.c
+++ lpm.c
@@ -3,24 +3,37 @@
 #include <string.h>
 #include <stdio.h>
 #include <stddef.h>
-
 #include <arpa/inet.h>
 
 
 /********************************* structs *********************************/
+typedef struct lpm_trie_node_s {
+    struct lpm_trie_node_s *next[2];
+    bool is_prefix;
+	char * prefix;
+} lpm_trie_node_t;
 
+typedef struct lpm_trie_s {
+    lpm_trie_node_t *root;
+    lpm_trie_node_t *ipv4_node;
+} lpm_trie_t;
 
 
 /********************************* attributes *********************************/
-const int LPM_MAX_PREFIX = 128;
+static const int LPM_MAX_PREFIX = 128;
 
 
-/***************************** private members **********************************/
-static unsigned int char_to_index(char c) {
-	return (int)c - (int)'a';
-}
+/***************************** private function declare **********************************/
+static int __ip_to_bin_str(const char *cidr, char *addr, size_t *len, unsigned int *prefix_len);
 
-static int ip_to_bin_str(const char *cidr, char *addr, unsigned int *prefix_len)
+static lpm_trie_node_t * __create_node();
+
+static int __is_empty(lpm_trie_node_t *root);
+static lpm_trie_node_t * __recursive_delete(lpm_trie_node_t *root, const char *key, int depth);
+
+
+/***************************** private function definition **********************************/
+static int __ip_to_bin_str(const char *cidr, char *addr, size_t *len, unsigned int *prefix_len)
 {
 	char *p;
 	char buf[INET6_ADDRSTRLEN];
@@ -50,6 +63,7 @@ static int ip_to_bin_str(const char *cidr, char *addr, unsigned int *prefix_len)
 			}
 		}
 
+		*len = 16;
 		return 0;
 	}
 
@@ -65,62 +79,14 @@ static int ip_to_bin_str(const char *cidr, char *addr, unsigned int *prefix_len)
 			addr[cnt++] = ((addr_buf[0] >> (31 - i)) & 1) + '0';
 		}
 
+		*len = 4;
 		return 0;
 	}
 
 	return 1;
 }
 
-static int is_empty(lpm_trie_node_t *root)
-{
-    for (int i = 0; i < 2; ++i) {
-		if (root->next[i]) {
-			return 0;
-		}
-	}
-    return 1;
-}
-
-lpm_trie_node_t * recursive_delete(lpm_trie_node_t *root, const char *key, int depth)
-{
-    if (!root) {
-		return NULL;
-	}
-
-    // If last character of key is being processed
-    if (depth == strlen(key)) {
-        // This node is no more end of word after removal of given key
-        if (root->is_prefix) {
-			free(root->prefix);
-			root->prefix = NULL;
-			root->is_prefix = false;
-		}
-
-        // If given is not prefix of any other word
-        if (is_empty(root)) {
-            free(root);
-            root = NULL;
-        }
-
-        return root;
-    }
-
-    // If not last character, recur for the child obtained using ASCII value
-    int index = key[depth] - '0';
-    root->next[index] = recursive_delete(root->next[index], key, depth + 1);
-
-    // If root does not have any child (its only child got deleted), and it is not end of another word.
-    if (is_empty(root) && root->is_prefix == false) {
-        free(root);
-        root = NULL;
-    }
-
-    return root;
-}
-
-
-/********************************* APIs *********************************/
-lpm_trie_node_t * lpm_create_node()
+static lpm_trie_node_t * __create_node()
 {
 	lpm_trie_node_t *node = (lpm_trie_node_t *) malloc(sizeof(lpm_trie_node_t)); 
 
@@ -136,25 +102,108 @@ lpm_trie_node_t * lpm_create_node()
     return node; 
 }
 
-lpm_return_type_e lpm_insert(lpm_trie_node_t *root, const char *key)
+static int __is_empty(lpm_trie_node_t *root)
 {
+    for (int i = 0; i < 2; ++i) {
+		if (root->next[i]) {
+			return 0;
+		}
+	}
+    return 1;
+}
+
+static lpm_trie_node_t * __recursive_delete(lpm_trie_node_t *root, const char *key, int depth)
+{
+    if (!root) {
+		return NULL;
+	}
+
+    // If last character of key is being processed
+    if (depth == strlen(key)) {
+        // This node is no more end of word after removal of given key
+        if (root->is_prefix) {
+			free(root->prefix);
+			root->prefix = NULL;
+			root->is_prefix = false;
+		}
+
+        // If given is not prefix of any other word
+        if (__is_empty(root)) {
+            free(root);
+            root = NULL;
+        }
+
+        return root;
+    }
+
+    // If not last character, recur for the child obtained using ASCII value
+    int index = key[depth] - '0';
+    root->next[index] = __recursive_delete(root->next[index], key, depth + 1);
+
+    // If root does not have any child (its only child got deleted), and it is not end of another word.
+    if (__is_empty(root) && root->is_prefix == false) {
+        free(root);
+        root = NULL;
+    }
+
+    return root;
+}
+
+
+/********************************* APIs *********************************/
+void * lpm_trie_init()
+{
+	lpm_trie_t *trie = (lpm_trie_t *) malloc(sizeof(lpm_trie_t));
+	trie->root = __create_node();
+
 	// convert to binary
 	char key_binary[129] = {'\0'};
+	size_t key_binary_len = 0;
 	unsigned int prefix_len;
-    if(ip_to_bin_str(key, key_binary, &prefix_len) != 0) {
+    __ip_to_bin_str("0:0:0:0:0:ffff::/96", key_binary, &key_binary_len, &prefix_len);
+
+	int length = strlen(key_binary);
+    int index;
+    lpm_trie_node_t *cur_node = trie->root;
+
+    for(int i = 0; i < length; ++i)
+    {
+		index = key_binary[i] - '0';
+
+        if (!cur_node->next[index]) {
+			cur_node->next[index] = __create_node();
+		}
+
+        cur_node = cur_node->next[index];
+    }
+
+	trie->ipv4_node = cur_node;
+
+    return (void *)trie; 
+}
+
+lpm_return_type_e lpm_insert(void *trie_hadnler, const char *key)
+{
+	lpm_trie_t *trie = (lpm_trie_t *)trie_hadnler;
+
+	// convert to binary
+	char key_binary[129] = {'\0'};
+	size_t key_binary_len = 0;
+	unsigned int prefix_len;
+    if(__ip_to_bin_str(key, key_binary, &key_binary_len, &prefix_len) != 0) {
 		return LPM_FAILED;
 	}
 
-    int length = strlen(key_binary); 
+    int length = strlen(key_binary);
     int index;
-    lpm_trie_node_t *cur_node = root; 
+    lpm_trie_node_t *cur_node = (key_binary_len == 16) ? trie->root : trie->ipv4_node;
 
-    for(int i = 0; i < length; ++i) 
+    for(int i = 0; i < length; ++i)
     {
 		index = key_binary[i] - '0';
  
         if (!cur_node->next[index]) {
-			cur_node->next[index] = lpm_create_node();
+			cur_node->next[index] = __create_node();
 		}
 
         cur_node = cur_node->next[index]; 
@@ -170,21 +219,24 @@ lpm_return_type_e lpm_insert(lpm_trie_node_t *root, const char *key)
 	return LPM_SUCCESS;
 }
 
-lpm_return_type_e lpm_search(lpm_trie_node_t *root, const char *key, const char **value)
+lpm_return_type_e lpm_search(void *trie_hadnler, const char *key, const char **value)
 {
+	lpm_trie_t *trie = (lpm_trie_t *)trie_hadnler;
+
 	*value = NULL;
 
 	// convert to binary
 	char key_binary[129] = {'\0'};
+	size_t key_binary_len = 0;
 	unsigned int prefix_len;
-	if(ip_to_bin_str(key, key_binary, &prefix_len) != 0) {
+	if(__ip_to_bin_str(key, key_binary, &key_binary_len, &prefix_len) != 0) {
 		return LPM_FAILED;
 	}
 
 	int cnt = 0;
     int length = strlen(key_binary);
     int index;
-    lpm_trie_node_t *cur_node = root;
+    lpm_trie_node_t *cur_node = (key_binary_len == 16) ? trie->root : trie->ipv4_node;
 
     while(cur_node != NULL && cnt < length)
     {
@@ -212,18 +264,22 @@ lpm_return_type_e lpm_search(lpm_trie_node_t *root, const char *key, const char 
 	return LPM_SUCCESS;
 }
 
-lpm_return_type_e lpm_delete(lpm_trie_node_t *root, const char *key)
+lpm_return_type_e lpm_delete(void *trie_hadnler, const char *key)
 {
+	lpm_trie_t *trie = (lpm_trie_t *)trie_hadnler;
+
 	// convert to binary
 	char key_binary[129] = {'\0'};
+	size_t key_binary_len = 0;
 	unsigned int prefix_len;
-    if(ip_to_bin_str(key, key_binary, &prefix_len) != 0) {
+    if(__ip_to_bin_str(key, key_binary, &key_binary_len, &prefix_len) != 0) {
 		return LPM_FAILED;
 	}
 
 	// determine if key exists
     int length = strlen(key_binary);
     int index;
+	lpm_trie_node_t *root = (key_binary_len == 16) ? trie->root : trie->ipv4_node;
     lpm_trie_node_t *cur_node = root;
     for(int i = 0; i < length; ++i)
     {
@@ -237,7 +293,7 @@ lpm_return_type_e lpm_delete(lpm_trie_node_t *root, const char *key)
 		return LPM_FAILED;
 	}
 
-	recursive_delete(root, key_binary, 0);
+	__recursive_delete(root, key_binary, 0);
 
 	return LPM_SUCCESS;
 }
--- lpm.h
+++ lpm.h
@@ -1,32 +1,21 @@
 #ifndef _LPM_H_
 #define _LPM_H_
 
-
 #include <stdlib.h>
 #include <stdbool.h>
-
-
-typedef struct lpm_trie_node_s {
-    struct lpm_trie_node_s *next[2];
-    bool is_prefix;
-	char * prefix;
-    
-} lpm_trie_node_t;
 
 typedef enum lpm_return_type_e {
     LPM_SUCCESS = 0,
     LPM_FAILED = 1,
 } lpm_return_type_e;
 
-
 /********************************* APIs *********************************/
-lpm_trie_node_t * lpm_create_node();
+void * lpm_trie_init();
 
-lpm_return_type_e lpm_insert(lpm_trie_node_t *root, const char *key);
+lpm_return_type_e lpm_insert(void *trie_hadnler, const char *key);
 
-lpm_return_type_e lpm_search(lpm_trie_node_t *root, const char *key, const char **value);
+lpm_return_type_e lpm_search(void *trie_hadnler, const char *key, const char **value);
 
-lpm_return_type_e lpm_delete(lpm_trie_node_t *root, const char *key);
-
+lpm_return_type_e lpm_delete(void *trie_hadnler, const char *key);
 
 #endif
